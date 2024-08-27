d3489c49c2a02b3101d239398a00d8b47d28f3fe
kis0062195 <kis0062195@gmail.com>
2024-08-27 14:46:35 +0900

    Update : README

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict():
     #3. 모델 결과 분석
     result = ai_process(sequence_data)
     if result == 1:
-        vul_text = create_sliding_windows(sequence_data,100,100,tok)
+        vul_text = create_sliding_windows(sequence_data,100,100,tok) 
         a = vul_c(vul_text,dbg_dict)
         return jsonify({"vul" : 1, "lines" : a})
     else:
